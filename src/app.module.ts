8fd2c16301ccf8f51f9a19ac9312d222d40f4128
Your Name~Manie2005 <you@example.com>
2024-09-03 17:16:15 +0100

    NEW

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,5 +7,6 @@ import { DatabaseModule } from './database/database.module';
   imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
+  
 })
 export class AppModule {}
