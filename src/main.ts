46d1ccbeb6050fd5c29f195c7bd207a4393276c0
Your Name~Manie2005 <you@example.com>
2024-09-04 14:38:40 +0100

    next commiter

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   }))
   app.use(cookieParser())
   app.setGlobalPrefix('api/v1')
-  const port = process.env.PROJECT_PORT;
+  const port = process.env.PROJECT_PORT || 80;
   await app.listen(port,()=>{
     console.log(`project running on port ${port}`)
   });
